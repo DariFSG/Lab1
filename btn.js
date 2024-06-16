9002d1b8e81945eff7afb0782f616b1d11231e1c
DariFSG <119135633+DariFSG@users.noreply.github.com>
2024-06-17 02:52:45 +0300

    lastchange

---
+++ btn.js
@@ -0,0 +1,98 @@
+// Через атрибут
+// Функція для обробки відправки форми через кнопку
+function handleFormSubmit(event) {
+    // Запобігаємо стандартній дії відправки форми
+    event.preventDefault(); 
+
+    console.log("Obrobnik podii Atrybut:");
+    console.log("Current Target:", event.currentTarget);
+
+    // Отримання значень полів форми
+    var initials = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var order = document.getElementById('comment').value;
+
+    // Приклад виводу в консоль
+    console.log("Initials:", initials);
+    console.log("Email:", email);
+    console.log("Phone:", phone);
+    console.log("Order:", order);
+}
+
+// Через властивість
+document.addEventListener('DOMContentLoaded', function() {
+    // Отримання кнопки по ідентифікатору
+    var submitButton = document.getElementById('submitButton');
+
+    // Обробник події через addEventListener
+    submitButton.addEventListener('click', function(event) {
+        // Запобігаємо стандартній дії відправки форми
+        event.preventDefault();
+
+        console.log("Obrobnik podii Vlastyvist:");
+        console.log("Current Target:", event.currentTarget);
+
+        // Отримання значень полів форми
+        var initials = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var phone = document.getElementById('phone').value;
+        var order = document.getElementById('comment').value;
+
+        // Приклад виводу в консоль
+        console.log("Initials:", initials);
+        console.log("Email:", email);
+        console.log("Phone:", phone);
+        console.log("Order:", order);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Отримання кнопки по ідентифікатору
+    var submitButton = document.getElementById('submitButton');
+
+    // Оголошення об'єкта, який буде обробляти події
+    var eventHandler = {
+        handleEvent: function(event) {
+            // Запобігаємо стандартній дії відправки форми
+            event.preventDefault();
+
+            console.log("Obrobnik podii handleEvent:");
+            console.log("Current Target:", event.currentTarget);
+
+            // Отримання значень полів форми
+            var initials = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var phone = document.getElementById('phone').value;
+            var order = document.getElementById('comment').value;
+
+            // Приклад виводу в консоль
+            console.log("Initials:", initials);
+            console.log("Email:", email);
+            console.log("Phone:", phone);
+            console.log("Order:", order);
+        }
+    };
+    // Обробник події через метод handleEvent
+    submitButton.addEventListener('click', eventHandler);
+});
+
+document.getElementById('list').onclick = function(event) {
+    var target = event.target;
+
+    if (target.tagName === 'li') {
+        var listItems = document.querySelectorAll('#list li');
+        listItems.forEach(function(item) {
+            item.classList.remove('highlight');
+        });
+
+        target.classList.add('highlight');
+    }
+};
+
+document.getElementById('menu').onclick = function(event) {
+    var target = event.target;
+    var action = target.dataset.action;
+
+    console.log('Action:', action);
+};
---
+++ script.js
@@ -0,0 +1,69 @@
+/*function Dialog() {
+    alert("Welcome to the Handmade Products!");
+    alert("Before we start, we want to ask something about you)");
+
+    let username = prompt("What is your name?");
+    if (username) {
+        let age = prompt("How old are you?");
+        if (age) {
+            let interests = prompt("What do you want to find here?");
+            if (interests) {
+                let message = `Hello, ${username}! You are ${age} years old and you want to find ${interests} here. Is that correct?`;
+                let correct = confirm(message);
+                if (correct) {
+                    alert("Thank you for the confirmation! We hope you will enjoy your time here and find what you are looking for!");
+                } else {
+                    alert("Please reload the page and try again. Thank you!");
+                }
+            }
+            else {
+                alert("Interests are not provided.");
+            }
+        } else {
+            alert("Age is not provided.");
+        }
+    } else {
+        alert("Name is not provided.");
+    }
+}
+Dialog();
+
+function info(firstName, lastName, position) {
+    let info = `Developer Info:\nFirst Name: ${firstName}\nLast Name: ${lastName}\nPosition: ${position}`;
+    alert(info);
+}
+info("Daria", "Semena", "Student Developer");
+
+function compare() {
+    str1 = prompt("First:");
+    str2 = prompt("Second:");
+    if (str1.length > str2.length) {
+        alert(`The larger string is: ${str1}`);
+    } else if (str1.length < str2.length) {
+        alert(`The larger string is: ${str2}`);
+    } else {
+        alert("Strings are equal.");
+    }
+}*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    /*const containers = document.querySelectorAll('.container');
+    for (const container of containers) {
+
+        alert('innerHTML: ' + container.innerHTML);
+
+        alert('outerHTML: ' + container.outerHTML);
+
+        alert('textContent: ' + container.textContent);
+
+        const textNode = container.firstChild;
+        if (textNode.nodeType === Node.TEXT_NODE) {
+            alert('nodeValue: ' + textNode.nodeValue);
+        }
+        else {
+            alert('The first child is not a text node!');
+        }
+        container.remove();
+    }*/     
+});
+
